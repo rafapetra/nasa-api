710d191483457ce03548e71cf96feedecbf195eb
Rafael Petrachini <raffp@me.com>
2022-10-16 11:03:55 -0700

    Add .env to git ignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // User Interface Logic 
-import { Constructor, Constructor2 } from './constructor.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-const constructor = new Constructor(3, 3, 3);
